a5c3568d04bbcea7c2b19c89cc0bdf31b519b598
Cooper <124405920+Madilyncooper@users.noreply.github.com>
2023-03-30 20:57:55 -0700

    Added fetch request for Pexels Api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,21 @@
 var memeApiKey =  'jzeomPx5iwUnwAU/a96sUQ==UdtVN88itkhPAA9i';
-var pexelApiKey = 'w2sIrwpKk7Gw4JfF7ws129C25twSIzoVDSKvO0JLYw3ZhrxSTKIsdVXU';
+var pexelsApiKey = 'w2sIrwpKk7Gw4JfF7ws129C25twSIzoVDSKvO0JLYw3ZhrxSTKIsdVXU';
+
+
+
+async function searchPhotos(query){
+    const data=await fetch(`https://api.pexels.com/v1/search?query=${query}`, 
+    {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            Authorization: pexelsApiKey,
+        },
+    });
+    const response=await data.json();
+    console.log(response);
+
+   // displayImages(response) ADD IN LATER
+};
+
+searchPhotos('red');
